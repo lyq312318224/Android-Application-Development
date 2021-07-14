f59914cf71d58a4cc5376de6f8efc845c3bd76a4
lyq312318224 <312318224@qq.com>
2021-07-14 19:30:33 +0800

    增加了作业3

---
+++ homework3/app/src/main/java/com/example/chapter3/homework/TestData.java
@@ -0,0 +1,17 @@
+package com.example.chapter3.homework;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+public class TestData {
+    String title;
+    String message;
+    String picname;
+    public TestData(String title,String message,String picname) {
+        this.title = title;
+        this.message=message;
+        this.picname=picname;
+        //this.imageView.setImageResource(R.drawable.p1);
+    }
+
+}
